6322fe9adacd6baa3f8fec23767b142d24ea29c8
Bend-Function <bendfunction@outlook.com>
2020-02-27 21:04:16 +0800

    增加了退格功能,以免认定不合格

---
+++ .history/fuck_20200227204306.py
@@ -0,0 +1,2 @@
+John Smith arrived to a small town on Friday.  He stayed there for two days and left  on Friday.  How is this possible? 
+ Answer: puzzle = “John Smith arrived to a small town on Friday.  He stayed there for two  days and left on Friday.  How is this possible?”      replacement = input(      newPuzzle = puzzle.__________________      print(                                 )  
---
+++ .history/fuck_20200227204400.py
@@ -0,0 +1,14 @@
+'''
+@Descripttion: 
+@version: 
+@Author: Bend Function
+@Date: 2020-02-27 20:42:55
+@LastEditors: Bend Function
+@LastEditTime: 2020-02-27 20:44:00
+'''
+# John Smith arrived to a small town on Friday.  He stayed there for two days and left  on Friday.  How is this possible? 
+Answer: puzzle = “John Smith arrived to a small town on Friday.  He stayed there for two  days and left on Friday.  How is this possible?”      
+replacement = input()   
+     
+newPuzzle = puzzle.replae     
+print(                                 )  
