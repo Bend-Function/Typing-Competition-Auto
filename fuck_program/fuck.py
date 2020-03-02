72f18e915ce23b47c584f7d59e50a7efc33e83c5
Bend-Function <bendfunction@outlook.com>
2020-03-02 15:04:34 +0800

    优化稳定性,优化目标WPM实现

--- fuck_program/fuck.py
+++ fuck_program/fuck.py
@@ -4,7 +4,7 @@
 @Author: Bend Function
 @Date: 2020-03-02 12:15:39
 @LastEditors: Bend Function
-@LastEditTime: 2020-03-02 14:22:44
+@LastEditTime: 2020-03-02 15:02:57
 '''
 import time
 from selenium import webdriver
@@ -12,19 +12,21 @@ from selenium.webdriver.common.keys import Keys
 import random
 
 #config--------------------------------
-# 退格率 填百分比,>1或=0
+# 退格率 填百分比
 BackPro = 1
+
 # 目标WPM
 TgWpm = 200
 
-#config--------------
-WpmDelay = 60 / TgWpm
+#config--------------------------------
+# 因为有退格,所以补偿些时间
+WpmDelay = 55 / TgWpm
 
 # 使用Firefox
 driver = webdriver.Firefox()
 # 打开比赛网页
 # A68X1
-# 
+
 driver.get('https://dazi.kukuw.com/')
 time.sleep(5)
 
@@ -37,6 +39,7 @@ for id in range(0,300):
     div_s = page_source.find("<div id=\"i_"+str(id)+"\"",start)
     if div_s == -1:
         break 
+    # 定位文字文字
     wordStart = page_source.find("<span>",div_s) + 6
     wordEnd = page_source.find("</span>",div_s)
     word = page_source[wordStart:wordEnd]
@@ -48,18 +51,19 @@ for line in list:
     # 这行要打的字 = line
     for word in line:
         # delay calc with target WPM
-        time.sleep(random.uniform(WpmDelay-0.1,WpmDelay+0.01))     # TODO
+        time.sleep(random.uniform(WpmDelay-0.2,WpmDelay+0.2))     # TODO
         driver.switch_to.active_element.send_keys(word)
-        # time.sleep(random.uniform(WpmDelay-0.01,WpmDelay+0.001))
         
-        # 退格控制
-        if random.uniform(0,100) <= BackPro:
+        # 退格控制,最后一个字不退,以防bug
+        if random.uniform(0,100) <= BackPro and word != line[len(line)-1]:
+            time.sleep(0.05)
             driver.switch_to.active_element.send_keys(Keys.BACKSPACE)
-            time.sleep(0.1)
+            time.sleep(0.05)
             driver.switch_to.active_element.send_keys(word)
-            time.sleep(0.1)
+            time.sleep(0.05)
         
         if word == line[len(line)-1]:
+            time.sleep(0.05)
             driver.switch_to.active_element.send_keys(Keys.RETURN)
-            time.sleep(0.1)
+            time.sleep(0.05)
  
