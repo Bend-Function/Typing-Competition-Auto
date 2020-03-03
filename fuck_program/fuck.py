23ce878d438f06332f5c6c13cdb1d8b88717e364
Bend-Function <bendfunction@outlook.com>
2020-03-03 12:56:45 +0800

    增加稳定性,优化代码结构

--- fuck_program/fuck.py
+++ fuck_program/fuck.py
@@ -4,7 +4,7 @@
 @Author: Bend Function
 @Date: 2020-03-02 12:15:39
 @LastEditors: Bend Function
-@LastEditTime: 2020-03-02 22:23:36
+@LastEditTime: 2020-03-03 12:55:54
 '''
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -17,18 +17,18 @@ logging.basicConfig(level=logging.WARNING)
 # 退格率 填百分比
 BackPro = 1
 
-# 目标WPM
-CnTgWpm = 100
+# 目标WPM,中文不宜超过200
+CnTgWpm = 180
 EnTgWpm = 300
 
 # 在进行退格\换行时的等待时间,默认0.05s
-normalDelay = 0.05
+normalDelay = 0.1
 
 # WPM波峰,波谷
 speedUp = 50
-speedDown = 50
+speedDown = 100
 # 波动范围
-floatPercent = 0.3
+floatPercent = 0.5
 # 曲线生成频率,默认5s
 speedFrq = 5
 #config--------------------------------
@@ -45,13 +45,10 @@ EnWords = 0
 # 判断句子是否为中文.
 # 中文返回1,英文返回0
 # s 中只要含中文即返回1
-def isCn(s):
-    Character = s
+def isCn(Character):
     for i in range(0,len(Character)):
         if Character[i] >= u'\u4e00' and Character[i]<=u'\u9fa5':
             return 100
-            i = i - 1
-            break
     if i == len(Character)-1:
         return 0
 
@@ -67,32 +64,16 @@ def blockSpeed(numBlocks,type):
     # 生成阶段曲线
     block = []
     # count = 0
-    for blockNum in range(0,numBlocks):
-        
-        
+    for blockNum in range(0,numBlocks):       
         # 为了营造波动幅度打的曲线,现在先随机方向
         direction = random.randint(0,1)
         if direction:
             ranSpeed = TgWpm + speedUp - random.randint(0,int(speedUp * floatPercent))
         else:
             ranSpeed = TgWpm - speedDown + random.randint(0,int(speedDown * floatPercent))
-        # count += ranSpeed
-        # # 最后一个补全平均数
-        # if blockNum == numBlocks - 1:
-        #     print("here")
-        #     block.append(int(numBlocks * TgWpm - count))
-        #     break
+
         block.append(ranSpeed)
     
-
-    # avg = 0
-    # for x in block:
-    #     avg += x
-
-    # print(len(block))
-    # print(block)
-    # print(avg / len(block))    
-        
     return block
 
 #查看当前剩余时间   秒
@@ -161,30 +142,24 @@ timeSurplusBefore = totalTimeLimit
 for numLines in range(0,len(list)):
     logging.warning("line %d",numLines)
     line = list[numLines][0]
-    if list[numLines][1]:
-        delay = 55 / CnBlocks[CnWriteNow]
-        CnWriteNow += 1
-    else:
-        delay = 55 / EnBlocks[EnWriteNow]
-        EnWriteNow += 1
-    # 这行要打的字 = line
-    for wordNum in range(0, len(line),2):
-        word = line[wordNum:wordNum+2]
+
+    for word in line:
         page_source =  driver.page_source
         timeSurplusNow = timeLimitNow(page_source)
-        if timeSurplusBefore - 5 >= timeSurplusNow:
-            logging.warning("Change Speed-CN %d EN %d",CnWriteNow,EnWriteNow)
+        if timeSurplusBefore - 5 > timeSurplusNow or timeSurplusBefore == totalTimeLimit:
             if list[numLines][1]:
-                delay = 110 / CnBlocks[CnWriteNow]
+                delay = 60 / CnBlocks[CnWriteNow]
                 CnWriteNow += 1 
             else:
-                delay = 110 / EnBlocks[EnWriteNow]
+                delay = 60 / EnBlocks[EnWriteNow]
                 EnWriteNow += 1
+            logging.warning("Change Speed-CN %d EN %d speed %d",CnWriteNow,EnWriteNow,int(delay*60))
+
             timeSurplusBefore = timeSurplusNow
 
         # delay calc with target WPM
-        time.sleep(random.uniform(delay-0.1,delay+0.1))     # TODO
-        # time.sleep(delay)
+        # time.sleep(random.uniform(delay-0.1,delay+0.1))     # TODO
+        time.sleep(delay)
         driver.switch_to.active_element.send_keys(word)
         
         # 退格控制,最后一个字不退,以防bug
@@ -192,8 +167,8 @@ for numLines in range(0,len(list)):
             time.sleep(normalDelay)
             driver.switch_to.active_element.send_keys(Keys.BACKSPACE)
             time.sleep(normalDelay)
-            driver.switch_to.active_element.send_keys(Keys.BACKSPACE)
-            time.sleep(normalDelay)
+            # driver.switch_to.active_element.send_keys(Keys.BACKSPACE)
+            # time.sleep(normalDelay)
             driver.switch_to.active_element.send_keys(word)
             time.sleep(normalDelay)
 
